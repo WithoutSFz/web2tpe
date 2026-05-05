9a410bafb8c4110c979a0eda6bcf35c69e04310f
Milagros Montero Larricq <milagrosmontero2005@gmail.com>
2026-05-05 19:57:24 -0300

    cambio en el sql

--- model/model.php
+++ model/model.php
@@ -48,6 +48,26 @@ class Model {
             `id_autor` int(11) NOT NULL
             ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+            -- --------------------------------------------------------
+
+            --
+            -- Estructura de tabla para la tabla `usuarios`
+            --
+
+            CREATE TABLE `usuarios` (
+            `id_usuario` int(11) NOT NULL,
+            `email` varchar(100) NOT NULL,
+            `password` varchar(255) NOT NULL,
+            `rol` varchar(50) NOT NULL
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+            --
+            -- Volcado de datos para la tabla `usuarios`
+            --
+
+            INSERT INTO `usuarios` (`id_usuario`, `email`, `password`, `rol`) VALUES
+            (1, 'webadmin', '\$2y\$10\$X0uGZPuF2KrPk5IrOFQMrOITK.A47HKGO8Z202eNap9zZE.FgtCPS', 'admin');
+
             --
             -- Índices para tablas volcadas
             --
@@ -63,7 +83,13 @@ class Model {
             --
             ALTER TABLE `libros`
             ADD PRIMARY KEY (`id_libro`),
-            ADD KEY `id_autor` (`id_autor`);
+            ADD UNIQUE KEY `id_autor` (`id_autor`);
+
+            --
+            -- Indices de la tabla `usuarios`
+            --
+            ALTER TABLE `usuarios`
+            ADD PRIMARY KEY (`id_usuario`);
 
             --
             -- AUTO_INCREMENT de las tablas volcadas
@@ -73,13 +99,19 @@ class Model {
             -- AUTO_INCREMENT de la tabla `autores`
             --
             ALTER TABLE `autores`
-            MODIFY `id_autor` int(11) NOT NULL AUTO_INCREMENT;
+            MODIFY `id_autor` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
             --
             -- AUTO_INCREMENT de la tabla `libros`
             --
             ALTER TABLE `libros`
             MODIFY `id_libro` int(11) NOT NULL AUTO_INCREMENT;
+
+            --
+            -- AUTO_INCREMENT de la tabla `usuarios`
+            --
+            ALTER TABLE `usuarios`
+            MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
             --
             -- Restricciones para tablas volcadas
